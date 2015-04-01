f71f6ce35d3031caa46f0f776b8ed5e784dc19d1
julianje <jjara@mit.edu>
2015-04-01 15:05:36 -0400

    Minor changes

--- Bunny/Bootstrapper/server.R
+++ Bunny/Bootstrapper/server.R
@@ -14,30 +14,43 @@ shinyServer(function(input, output){
     return(df)
   })
   
+  output$varx = renderUI({
+    if (is.null(input$file1)) { return() }
+    selectInput('varx', 'Variable 1', names(Data()))
+  })
+
+  output$vary = renderUI({
+    if (is.null(input$file1)) { return() }
+    selectInput('vary', 'Variable 2', names(Data()))
+  })
+  
+  output$summary <- renderPrint({
+    if (is.null(input$file1)) { return() }
+    summary(Data()[,c(input$varx)])
+  })
+  
   output$distPlot <- renderPlot({
     
     if (is.null(input$file1)) { return() }
+    if (!(input$varx %in% names(Data()))){ return() }
 
     if (input$statistic=="Mean"){
-      if (input$varx==""){ return() }
-      Samples <- boot(Data(),function(x,id){return(mean(x[id,c(input$varx)]))},input$Samples)
+      Samples <- boot(Data(),function(x,id){return(mean(x[id,c(input$varx)],na.rm=T))},input$Samples)
+      TitleA=paste("Bootstrapped samples of ",input$varx," mean\n")
     }
     if (input$statistic=="Correlation"){
-      if (input$varx==""){ return() }
-      if (input$vary==""){ return() }
-      Samples <- boot(Data(),function(x,id){return(cor(x[id,c(input$varx)],x[id,c(input$vary)]))},input$Samples)
+      Samples <- boot(Data(),function(x,id){return(cor(x[id,c(input$varx)],x[id,c(input$vary)],use="pairwise.complete.obs"))},input$Samples)
+      TitleA=paste("Bootstrapped samples of correlation between",input$varx,"and",input$vary,"\n")
     }
     if (input$statistic=="Mean difference"){
-      if (input$varx==""){ return() }
-      if (input$vary==""){ return() }
-      Samples <- boot(Data(),function(x,id){return(mean(x[id,c(input$varx)])-mean(x[id,c(input$vary)]))},input$Samples)
+      Samples <- boot(Data(),function(x,id){return(mean(x[id,c(input$varx)],na.rm=T)-mean(x[id,c(input$vary)],na.rm=T))},input$Samples)
+      TitleA=paste("Bootstrapped samples of mean difference between",input$varx,"and",input$vary,"\n")
     }
-    res = boot.ci(Samples)$basic
+    res = boot.ci(Samples,type="basic")$basic
     lower = res[4]
     upper = res[5]
-    # col = 'darkgray'
     hist(Samples$t, breaks = input$bins, col = 'skyblue', border = 'white',
          xlab="Statistic",ylab="Number of samples",
-         main=paste("95% CI: ",lower," - ",upper))
+         main=paste(TitleA,"95% CI: ( ",lower," , ",upper," )"))
   })
 })
--- Bunny/Bootstrapper/ui.R
+++ Bunny/Bootstrapper/ui.R
@@ -8,25 +8,27 @@ shinyUI(fluidPage(
   # Side bar
   sidebarLayout(
     sidebarPanel(
+      h4("Usage"),
+      p("Upload a csv file and choose two columns. Some statistics will use both columns; others only use the first one. Your data is deleted when you close the window."),
+      HTML('<a href="https://github.com/julianje/Bunny/blob/master/Bunny/Bootstrapper/ExampleData.csv">Download an example file.</a>'),
       fileInput('file1', 'Choose CSV File',
                 accept=c('text/csv', 
                          'text/comma-separated-values,text/plain', 
                          '.csv')),
-      textInput('varx', "Variable X", ""),
-      textInput('vary', "Variable Y", ""),
-      #selectInput('varx', 'Variable X', names(Data())),
-      #selectInput('vary', 'Variable Y', names(Data())),
+      uiOutput("varx"),
+      uiOutput("vary"),
       selectInput('statistic', 'Statistic', c("Mean","Correlation","Mean difference")),
-      numericInput('Samples','Number of samples', 10000, min=100, max=100000),
-      sliderInput('bins','Number of bins:',
+      numericInput('Samples','Number of bootstrap samples', 1000, min=100, max=100000),
+      sliderInput('bins','Adjust bins in plot',
                   min=1,
                   max=50,
-                  value=30)
+                  value=30),
+      HTML('Implemented by <a href="http://web.mit.edu/jjara/www/">Julian</a> and powered by R and shiny.')
       ),
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("distPlot"),
+      h2("Data summary of variable 1"),
+      verbatimTextOutput("summary")
     )
-  ),
-  img(src="shiny_logo.png"),
-  h6("Powered by R.")
+  )
 ))
